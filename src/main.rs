d3594019ea1783104fed2051267adc8949cc9a1c
ParadoxPD <ask.priyanshu@gmail.com>
2025-04-25 17:18:11 +0530

    VGA buffer module wokrs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -11,15 +13,7 @@ use bootloader::{BootInfo, entry_point};
 entry_point!(_start);
 
 fn _start(_boot_info: &'static BootInfo) -> ! {
-    let vga_buffer: *mut u8 = 0xb8000 as *mut u8;
-    let message = b"Hello World!!!!\n";
-
-    for (i, &byte) in message.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset((i * 2) as isize) = byte;
-            *vga_buffer.offset((i * 2 + 1) as isize) = 0x0f;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
