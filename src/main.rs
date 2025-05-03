25fc8fab309c04007d73f82fba0cb5d64ba7fc31
ParadoxPD <ask.priyanshu@gmail.com>
2025-05-03 21:23:48 +0530

    Print macro

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,16 @@
 
 mod vga_buffer;
 
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-use bootloader::{BootInfo, entry_point};
 entry_point!(_start);
-
 fn _start(_boot_info: &'static BootInfo) -> ! {
-    vga_buffer::print_something();
-
+    println!("Hello World");
     loop {}
 }
